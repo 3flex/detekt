0ce1b1ca206ac0e17422437dcb237f7076d291a5
Matthew Haughton <3flex@users.noreply.github.com>
2020-01-08 11:28:14 +1100

    Update build.gradle.kts

--- detekt-core/build.gradle.kts
+++ detekt-core/build.gradle.kts
@@ -1,6 +1,5 @@
 configurations.testImplementation.get().extendsFrom(configurations.kotlinTest.get())
 
-val junitPlatformVersion: String by project
 val spekVersion: String by project
 val reflectionsVersion: String by project
 
@@ -10,6 +9,5 @@ dependencies {
     testImplementation(project(":detekt-rules"))
     testImplementation(project(":detekt-test"))
     testImplementation("org.reflections:reflections:$reflectionsVersion")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
 }
